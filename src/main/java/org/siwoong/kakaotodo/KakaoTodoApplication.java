a7ff88150af111cdd9a954ae11c8c985d7a09da4
Lee Si Woong <silverttthin@naver.com>
2025-05-24 19:17:10 +0900

    init: 프로젝트 구축 + readme 생성

---
+++ src/main/java/org/siwoong/kakaotodo/KakaoTodoApplication.java
@@ -0,0 +1,15 @@
+package org.siwoong.kakaotodo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class KakaoTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/siwoong/kakaotodo/KakaoTodoApplicationTests.java
@@ -0,0 +1,15 @@
+package org.siwoong.kakaotodo;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class KakaoTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
