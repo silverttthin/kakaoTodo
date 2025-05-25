bf8f13f6b7822bc14eb7083e3e95261099ea1663
Lee Si Woong <silverttthin@naver.com>
2025-05-25 18:43:27 +0900

    feat: 검증 추가

--- src/main/java/org/siwoong/kakaotodo/todo/dto/DeleteTodoRequest.java
+++ src/main/java/org/siwoong/kakaotodo/todo/dto/DeleteTodoRequest.java
@@ -1,7 +1,12 @@
 package org.siwoong.kakaotodo.todo.dto;
 
 
+import jakarta.validation.constraints.NotNull;
+
+
 public record DeleteTodoRequest(
+
+	@NotNull(message = "암호는 필수입력값입니다.")
 	String password
 ) {
 }
--- src/main/java/org/siwoong/kakaotodo/todo/dto/UpdateTodoRequest.java
+++ src/main/java/org/siwoong/kakaotodo/todo/dto/UpdateTodoRequest.java
@@ -1,10 +1,15 @@
 package org.siwoong.kakaotodo.todo.dto;
 
 
+import jakarta.validation.constraints.NotBlank;
+
+
 public record UpdateTodoRequest(
+
+	@NotBlank(message = "암호는 필수입력값입니다")
 	String password,
 
-	String content,
-	String creator
+	@NotBlank(message = "내용은 필수입력값입니다")
+	String content
 ) {
 }
