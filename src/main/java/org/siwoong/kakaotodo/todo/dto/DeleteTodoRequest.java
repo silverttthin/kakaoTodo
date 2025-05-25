87d61ea18b71bfae7879d09d517ee77f55f7e444
Lee Si Woong <silverttthin@naver.com>
2025-05-25 18:51:03 +0900

    feat: req dto에 로그인했다 가정한 유저의 userId 추가

--- src/main/java/org/siwoong/kakaotodo/todo/dto/DeleteTodoRequest.java
+++ src/main/java/org/siwoong/kakaotodo/todo/dto/DeleteTodoRequest.java
@@ -6,6 +6,9 @@ import jakarta.validation.constraints.NotNull;
 
 public record DeleteTodoRequest(
 
+	@NotNull(message = "userId는 필수입력값입니다")
+	Long userId,
+
 	@NotNull(message = "암호는 필수입력값입니다.")
 	String password
 ) {
--- src/main/java/org/siwoong/kakaotodo/todo/dto/UpdateTodoRequest.java
+++ src/main/java/org/siwoong/kakaotodo/todo/dto/UpdateTodoRequest.java
@@ -2,9 +2,13 @@ package org.siwoong.kakaotodo.todo.dto;
 
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 
 public record UpdateTodoRequest(
+
+	@NotNull(message = "userId는 필수입력값입니다")
+	Long userId,
 
 	@NotBlank(message = "암호는 필수입력값입니다")
 	String password,
