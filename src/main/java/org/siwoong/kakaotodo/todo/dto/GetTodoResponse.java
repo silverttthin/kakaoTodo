11a400a43f535955b49efbd914c09d32e4c89ed2
Lee Si Woong <silverttthin@naver.com>
2025-05-25 21:02:51 +0900

    feat: userName, email에 검증조건 추가(notblank, email)

--- src/main/java/org/siwoong/kakaotodo/todo/dto/GetTodoResponse.java
+++ src/main/java/org/siwoong/kakaotodo/todo/dto/GetTodoResponse.java
@@ -1,6 +1,7 @@
 package org.siwoong.kakaotodo.todo.dto;
 
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,7 +18,7 @@ public class GetTodoResponse {
 	Integer userId;
 	@NotBlank
 	String userName;
-	@NotBlank
+	@NotBlank @Email
 	String userEmail;
 	String content;
 	LocalDateTime createdAt;
