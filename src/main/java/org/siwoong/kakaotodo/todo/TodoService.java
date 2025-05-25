aa963df8b0f0d13a42e2547ca5d14cf587c69caa
Lee Si Woong <silverttthin@naver.com>
2025-05-25 18:51:55 +0900

    refactor & comment: 타입 및 유저검증 추가 + 주석작업

--- src/main/java/org/siwoong/kakaotodo/todo/TodoService.java
+++ src/main/java/org/siwoong/kakaotodo/todo/TodoService.java
@@ -23,8 +23,18 @@ public class TodoService {
 	// jdbc 빈 가져오기
 	private final JdbcTemplate jdbc;
 
+	public void validateUserExist(Long userId) {
+		String checkUserSql = "SELECT COUNT(*) FROM users WHERE id = ?";
+		Integer cnt = jdbc.queryForObject(checkUserSql,Integer.class, userId);
+		if(cnt == 0) throw new RuntimeException("[USER ERROR]존재하지 않는 유저입니다");
+	}
+
 	// 투두 생성
 	public Integer create(CreateTodoRequest todo) {
+		// user가 있는지 검증
+		validateUserExist(todo.userId());
+
+		// 입력값 기반으로 생성 쿼리 생성 후 저장
 		String sql = "INSERT INTO todos (user_id, password, content, created_at, updated_at) VALUES (?, ?, ?, ?, ?)";
 		LocalDateTime now = LocalDateTime.now();
 		jdbc.update(sql,
@@ -38,24 +48,26 @@ public class TodoService {
 	}
 
 	// 단건 조회
-	public GetTodoResponse findById(Long id) {
+	public GetTodoResponse findById(Long todoId) {
 		String sql = """
 					    SELECT id, content, user_id AS userId, created_at AS createdAt, updated_at AS updatedAt
 					    FROM todos WHERE id = ?
 					""";
 
-		return jdbc.query(sql, new BeanPropertyRowMapper<>(GetTodoResponse.class), id)
+		return jdbc.query(sql, new BeanPropertyRowMapper<>(GetTodoResponse.class), todoId)
 			.stream().findFirst().orElseThrow(() -> {throw new RuntimeException("해당 id를 가진 todo 없음");});
 	}
 
 	// 전체 조회
 	public List<GetTodoResponse> findAll(Long userId) {
-		if (userId == null) { // 쿼리변수없으면 걍 수정일 기반 목록
+		if (userId == null) {
+			// 쿼리변수없으면 걍 수정일 기반 전체 목록
 			String sql = "SELECT id, user_id AS userId, content, created_at AS createdAt, updated_at AS updatedAt FROM todos"
 				+ " ORDER BY updated_at DESC";
 
 			return jdbc.query(sql, new BeanPropertyRowMapper<>(GetTodoResponse.class));
 		} else {
+			// userId가 있다면 해당 유저의 투두 가져오기
 			String sql = "SELECT id, user_id AS userId, content, created_at AS createdAt, updated_at AS updatedAt FROM todos"
 				+ " WHERE user_id = ?"
 				+ " ORDER BY updated_at DESC";
@@ -66,13 +78,16 @@ public class TodoService {
 
 	// 삭제
 	public void delete(Long todoId, DeleteTodoRequest request) {
+		// 유저 검증
+		validateUserExist(request.userId());
+
+		// 게시글 존재 검증 및 비번검증
 		String selectSql = "SELECT password FROM todos WHERE id = ?";
 		String dbPassword = jdbc.query(selectSql, (rs) -> rs.next() ? rs.getString("password") : null, todoId);
-
 		if (dbPassword == null) throw new RuntimeException("해당 id를 가진 todo 없음");
-
 		if(!dbPassword.equals(request.password())) throw new RuntimeException("비밀번호가 틀림");
 
+		// 삭제
 		String sql = "DELETE FROM todos WHERE id = ?";
 		jdbc.update(sql, todoId);
 	}
@@ -80,13 +95,19 @@ public class TodoService {
 
 	// 수정
 	public GetTodoResponse update(Long todoId, UpdateTodoRequest request) {
+
+		//유저 검증
+		validateUserExist(request.userId());
+
+		// 게시글 존재 검증 및 비번검증
 		String selectSql = "SELECT password FROM todos WHERE id = ?";
 		String dbPassword = jdbc.query(selectSql, (rs) -> rs.next() ? rs.getString("password") : null, todoId);
 		if (dbPassword == null) throw new RuntimeException("해당 id를 가진 todo 없음");
 		if (!dbPassword.equals(request.password())) throw new RuntimeException("비밀번호가 틀림");
 
-		String updateSql  = "UPDATE todos SET creator = ?, content = ?, updated_at = NOW() WHERE id = ?";
-		int rows = jdbc.update(updateSql, request.creator(), request.content(), todoId);
+		// 수정
+		String updateSql  = "UPDATE todos SET content = ?, updated_at = NOW() WHERE id = ?";
+		int rows = jdbc.update(updateSql, request.content(), todoId);
 		if(rows != 1) throw new RuntimeException("수정에 실패했습니다");
 
 		return findById(todoId);
